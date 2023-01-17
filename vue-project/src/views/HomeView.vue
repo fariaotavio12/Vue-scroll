c711ea5910b03ec548c37bbf006f985772e1dce9
Otávio Faria <67973520+fariaotavio12@users.noreply.github.com>
2023-01-17 09:42:38 -0300

    Scroll

---
+++ vue-project/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { vScrollReveal } from 'vue-scroll-reveal';
+
+// OR if using custom default options
+import { createScrollRevealDirective } from 'vue-scroll-reveal'; 
+const vScrollReveal = createScrollRevealDirective({
+    delay: 1000,
+    duration: 150,
+});
+</script>
+
+<template>
+  <main>
+    <div>asdadsasdad</div>
+  </main>
+</template>
